0ad42ecbea92519040f0154572d22b35304e0071
Kazım Anıl Eren <anil.eren@digitallency.com>
2018-05-10 16:52:02 +0300

    font change.

--- GGPlot_Theme.R
+++ GGPlot_Theme.R
@@ -7,15 +7,15 @@ theme_dt <- function(){
     axis.line    = element_line(colour = "black", lineend = "square", linetype = "solid"  , size = 0.3),
     axis.text.x  = element_text(size = 11, colour = "darkseagreen1", family = "Ubuntu"),
     axis.text.y  = element_text(size = 11, colour = "darkseagreen1", family = "Ubuntu"),
-    axis.title.x = element_text(size = 15, colour = "darkseagreen1", family = "Arial", margin = margin(0.5,0,0,0, "cm")),
-    axis.title.y = element_text(size = 15, colour = "darkseagreen1", family = "Arial", margin = margin(0,0.5,0,0, "cm"), angle = 90),
+    axis.title.x = element_text(size = 15, colour = "darkseagreen1", family = "Lato", margin = margin(0.5,0,0,0, "cm")),
+    axis.title.y = element_text(size = 15, colour = "darkseagreen1", family = "Lato", margin = margin(0,0.5,0,0, "cm"), angle = 90),
     axis.ticks   = element_line(colour = "darkseagreen1"),
     
     legend.background     = element_rect(fill = "darkslategray"), 
     legend.key            = element_rect(fill = "darkslategray"),
     legend.box.background = element_rect(fill = "darkslategray"),
     legend.text           = element_text(colour = "darkseagreen1", size = 11, family = "Ubuntu"),
-    legend.title          = element_text(colour = "darkseagreen1", size = 12, family = "Arial", margin = margin(0, 0.1, 0, 0, "cm")),
+    legend.title          = element_text(colour = "darkseagreen1", size = 12, family = "Lato", margin = margin(0, 0.1, 0, 0, "cm")),
     legend.position       = "right",
     legend.text.align     = 0.01,
     legend.title.align    = 0.5,
@@ -31,7 +31,7 @@ theme_dt <- function(){
     strip.text.y     = element_text(angle = -90),
     
     plot.background =  element_rect(fill = "darkslategray"),
-    plot.title      =  element_text(size = 20, colour = "darkseagreen1", family = "Arial", margin = margin(0,0,0.5,0, "cm")),
+    plot.title      =  element_text(size = 20, colour = "darkseagreen1", family = "Lato", margin = margin(0,0,0.5,0, "cm")),
     plot.margin     =  unit(c(1,1,0.5,0.5), "lines")
     )
 }
